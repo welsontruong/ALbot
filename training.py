87692a5fcd2f2d785092be93c8a82820f662af34
Welson Truong <welson.truong67@bcmail.cuny.edu>
2023-05-10 17:06:56 -0400

    changes

--- training.py
+++ training.py
@@ -12,12 +12,13 @@ from nltk.stem import WordNetLemmatizer
 #from tensorflow.keras.optimizers import SGD
 
 lemmatizer = WordNetLemmatizer()
-intents = json.loads(open('intents.json').read())
+
+intents = json.loads(open(r'C:\Users\megas\Documents\GitHub\ALbot\intents.json').read())
 
 words = []
 classes = []
 documents = []
-ignore_letters = [ '?' , '.' , ',' , '!']
+ignore_letters = [ '?' , '.' , ',' , '!','~','"']
 
 for intent in intents['intents']:
     for pattern in intent['patterns']:
@@ -52,19 +53,19 @@ for document in documents:
 random.shuffle(training)
 training = np.array(training)
 
-trainX = training[:,:len(words)]
-trainY = training[:,len(words):]
+trainX = training[:, :len(words)]
+trainY = training[:, len(words):]
 
 model = tf.keras.Sequential()
 model.add(tf.keras.layers.Dense(128,input_shape=(len(trainX[0]),), activation='relu'))
 model.add(tf.keras.layers.Dropout(0.5))
-model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dense(64, activation = 'relu'))
 model.add(tf.keras.layers.Dropout(0.5))
 model.add(tf.keras.layers.Dense(len(trainY[0]), activation='softmax'))
 
 sgd = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-model.fit(trainX, trainY, epochs=200, batch_size=5, verbose=1)
-model.save('chatbotmodel.h5')
+hist = model.fit(np.array(trainX), np.array(trainY), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5' ,hist)
 print('done')

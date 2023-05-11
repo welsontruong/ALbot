ab42b891e2b6b94075ec828cc364df1f977df240
Welson Truong <welson.truong67@bcmail.cuny.edu>
2023-05-11 16:17:49 -0400

    some changes

--- training.py
+++ training.py
@@ -70,6 +70,6 @@ model.add(tf.keras.layers.Dense(len(trainY[0]), activation='softmax'))
 sgd = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-hist = model.fit(np.array(trainX), np.array(trainY), epochs=200, batch_size=5, verbose=1)
+hist = model.fit(np.array(trainX), np.array(trainY), epochs=400, batch_size=5, verbose=1)
 model.save('chatbot_model.h5' , hist)
 print('done')
